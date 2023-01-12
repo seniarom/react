38078e19a633fe9634479f9288323efeb828764b
Kseniia <seniarom@gmail.com>
2023-01-12 13:36:55 +0300

    Homework8

---
+++ src/store/profile/actions.js
@@ -0,0 +1,17 @@
+import * as types from '../profile/types'
+
+// export const changeName = (data) => {
+//   return {
+//     type: types.CHANGE_NAME, 
+//     payload: data
+//   }
+// }
+
+export const changeName = (data) => ({
+  type: types.CHANGE_NAME,
+  payload: data
+})
+
+export const toggleProfile = () => ({
+  type: types.TOGGLE_PROFILE
+})
